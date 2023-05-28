e2894d0e735b0c1a8b7d0f12440eb92e71969cf1
Berke Dalar <berkedalar4@gmail.com>
2023-05-28 16:09:38 +0300

    New Store and Fix
    Added new stores and fixed pricing problems

--- Assets/gamemanager.cs
+++ Assets/gamemanager.cs
@@ -8,10 +8,12 @@ public class gamemanager : MonoBehaviour
     public GameObject storeObject1;
     public GameObject storeObject2;
     public GameObject storeObject3;
+    public GameObject storeObject4;
 
     private Store store1;
     private Store store2;
     private Store store3;
+    private Store store4;
     public Text CurrentBalanceText;
     public float CurrentBalance;
     // Start is called before the first frame update
@@ -22,13 +24,16 @@ public class gamemanager : MonoBehaviour
 
         store1 = storeObject1.GetComponent<Store>();
         store1.BaseStoreCost = 3.50f;
-        store1.BaseStoreProfit = 1.0f;
-        store2 = storeObject1.GetComponent<Store>();
+        store1.BaseStoreProfit = 1.0f; 
+        store2 = storeObject2.GetComponent<Store>();
         store2.BaseStoreCost = 5.50f;
         store2.BaseStoreProfit = 2.0f;
-        store3 = storeObject1.GetComponent<Store>();
+        store3 = storeObject3.GetComponent<Store>();
         store3.BaseStoreCost = 2.50f;
         store3.BaseStoreProfit = 1.2f;
+        store4 = storeObject4.GetComponent<Store>();
+        store4.BaseStoreCost = 1.50f;
+        store4.BaseStoreProfit = 1.5f;
     }
 
     // Update is called once per frame
